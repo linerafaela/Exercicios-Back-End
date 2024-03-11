9c4177aae4a5f32b90086447ddd2450ce1d6afb0
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-03-11 09:03:17 -0300

    Add files via upload

---
+++ votar.js
@@ -0,0 +1,11 @@
+// Variáveis representando idade e cidadania
+let idade = 18;
+let cidadania = true;
+
+// Verificar se a idade é maior ou igual a 18 e se a cidadania é brasileira (true)
+
+if (idade == 18 && cidadania) {
+    console.log ("Você pode votar!");
+} else {
+    console.log("Não pode votar!");
+}
