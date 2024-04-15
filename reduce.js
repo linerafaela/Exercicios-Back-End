ae35f1f59e909197e0eeb267aae6da79a2df2a62
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-04-15 08:57:33 -0300

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,3 @@
+const soma = [1,2.8,3,4].reduce ((acumulador, valorAtual) => acumulador + valorAtual);
+
+console.log(soma)
---
+++ repetido.js
@@ -0,0 +1,5 @@
+let apelidos = ['Totó','Cavalinho','Belinha','Amora','Dog caramelo','Carmelo'];
+
+let apelidos_unicos = [... new Set (apelidos)]
+
+console.log(apelidos_unicos)
